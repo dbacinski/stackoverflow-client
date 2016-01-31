c40a96d200630671824d28cc9434a119fa5ae4de
dariusz.bacinski <dariusz.bacinski@gmail.com>
2016-01-31 11:25:54 +0100

    Launching question details in browser

--- app/src/main/java/pl/dariuszbacinski/stackoverflow/search/viewmodel/QuestionItemViewModel.java
+++ app/src/main/java/pl/dariuszbacinski/stackoverflow/search/viewmodel/QuestionItemViewModel.java
@@ -3,10 +3,10 @@ package pl.dariuszbacinski.stackoverflow.search.viewmodel;
 import android.content.Intent;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.net.Uri;
 import android.view.View;
 
 import lombok.Data;
-import pl.dariuszbacinski.stackoverflow.search.view.SearchActivity;
 
 @Data
 public class QuestionItemViewModel extends BaseObservable {
@@ -26,7 +26,10 @@ public class QuestionItemViewModel extends BaseObservable {
 
     public void openDetails(View view) {
         //TODO send event to activity and then open details
-        //TODO launch details activity
-        view.getContext().startActivity(new Intent(view.getContext(), SearchActivity.class));
+        if (link != null && !link.isEmpty()) {
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(link));
+            view.getContext().startActivity(i);
+        }
     }
 }
